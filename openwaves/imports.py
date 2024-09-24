30edea9f5a882b537b62fcf2be91b4dae226eb62
Craig Troop <85651362+kr3kls@users.noreply.github.com>
2024-09-24 05:29:16 -0400

    HC Exam Session Registration
    Feature added to allow HC to register and cancel registration for exam sessions.

--- openwaves/imports.py
+++ openwaves/imports.py
@@ -3,6 +3,7 @@
     This file contains imports to break circular dependencies in the application.
 """
 
-from .models import User, Question, Pool, TLI, ExamSession # pylint: disable=W0611
-from .utils import update_user_password # pylint: disable=W0611
+from .models import User, Question, Pool, TLI, ExamSession, ExamRegistration # pylint: disable=W0611
+from .utils import update_user_password, get_exam_name, is_already_registered, \
+     remove_exam_registration # pylint: disable=W0611
 from . import db # pylint: disable=W0611
